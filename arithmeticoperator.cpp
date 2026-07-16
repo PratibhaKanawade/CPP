e3d4a03433fcce9802e7bbf2d69cdd5de9b0abc8
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-16 22:05:58 +0530

    added arithmetic operator program

---
+++ arithmeticoperator.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5,b=9;
+    //addition
+    cout<<"value of a+b:"<<a+b<<endl;
+    //substraction
+    cout<<"value of a-b:"<<a-b<<endl;
+    //multiplication
+    cout<<"value of a*b:"<<a*b<<endl;
+    //division
+    cout<<"value of a/b:"<<a/b<<endl;
+    //modulus
+    cout<<"value of a%b:"<<a%b<<endl;
+
+    return 0;
+
+}           
