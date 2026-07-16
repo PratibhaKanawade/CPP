2b99ae6fca3b29ba37ae48afad7b3447a630bb08
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-16 23:46:42 +0530

    updated bitwise operator program

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5, b=3;
+    //bitwise AND
+    cout<<"value of a&b is:"<<(a&b)<<endl;
+    //bitwise OR
+    cout<<"value of a|b is:"<<(a|b)<<endl;
+    //bitwise XOR
+    cout<<"value of a^b is:"<<(a^b)<<endl;
+    //bitwise left shift 
+    cout<<"value of a>>1 is:"<<(a>>1)<<endl;
+    //bitwise right shift 
+    cout<<"value of a<<1 is:"<<(a<<1)<<endl;
+    //bitwise NOT
+    cout<<"value of ~a is:"<<(~a)<<endl;
+    return 0;
+
+}           
