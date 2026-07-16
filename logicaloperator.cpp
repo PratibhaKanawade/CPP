8acc440b150a976991297372e7cbc25220a49106
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-16 23:16:40 +0530

    updated logical operator program

---
+++ logicaloperator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0,b=1;
+    //AND
+    cout<<"a&&b is:"<<(a&&b)<<endl;
+    //OR
+    cout<<"a||b is:"<<(a||b)<<endl;
+    //NOT
+    cout<<"!a is:"<<(!a)<<endl;
+
+    return 0;
+
+}           
