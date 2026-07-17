348d2d4816c5f2bf219eec78315273c9988ba1fc
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-18 00:41:49 +0530

    updated type casting

---
+++ typecasting.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int to float
+    int num1=10;
+    float num2=5.5;
+    float result1=num1+num2;
+    int result2=num1+num2;
+    cout<< result1<<endl;
+    cout<< result2<<endl;
+
+    //char to int
+    char ch='A';
+    int a=ch+1;//value of A is 65 so 65+1=66
+    cout<<a<<endl;
+    
+    //int to char
+    int a1=97;
+    char ch1=a;
+    cout<<a1<<endl;
+    return 0;
+
+}           
