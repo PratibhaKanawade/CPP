d1dc48589134b1ceb47ec1651604721630633e6e
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-18 18:07:49 +0530

    updated input output.cpp

---
+++ inputoutput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //variable declaration
+    int marks;
+    //for printing things on screen
+    cout<<"enter your marks"<<endl;
+    //for taking input
+    cin>>marks;
+    cout<<"your marks are:"<<marks;
+    return 0;
+
+}           
