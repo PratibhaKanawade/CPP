c936ea1a03443ac56fcb7c94d7a86e0ae59ff5f7
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-18 23:41:19 +0530

    updated inputoutput.cpp

--- inputoutput.cpp
+++ inputoutput.cpp
@@ -7,7 +7,14 @@ int main(){
     cout<<"enter your marks"<<endl;
     //for taking input
     cin>>marks;
-    cout<<"your marks are:"<<marks;
+    cout<<"your marks are:"<<marks<<endl;
+
+    //getline()
+    cin.ignore();
+    string name;
+    cout<<"enter your full name:"<<endl;
+    getline(cin,name);
+    cout<<"your name is:"<<name;
     return 0;
 
 }           
