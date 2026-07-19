dc4c4a8e726360e2af5de9a93876ce9c0e9f01f0
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-19 14:08:08 +0530

    updated ifelse program

---
+++ ifelse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int budget;
+    cout<<"enter your budget:"<<endl;
+    cin>>budget;
+    if(budget>200000){
+        cout<<"you can buy bike"<<endl;
+    }
+    else
+    {
+        cout<<"you cannot buy bike"<<endl;
+    }
+    return 0;
+
+}           
