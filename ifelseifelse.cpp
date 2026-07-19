bab0856c7c2fa9423592a2e459a414fa28fe0ce0
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-19 22:43:06 +0530

    updated ifelseifelse program

---
+++ ifelseifelse.cpp
@@ -0,0 +1,39 @@
+ #include<iostream>
+using namespace std;
+int main(){
+    int marks;
+    cout<<"enter your marks:";
+    cin>>marks;
+    if(marks>90){
+        cout<<"you own A grade";
+    }
+     else if(marks>80){
+        cout<<"you own B grade";
+    }
+    else  if(marks>70){
+        cout<<"you own C grade";
+    }
+     else if(marks>60){
+        cout<<"you own D grade";
+    }
+    else{
+        cout<<"you are failed";
+    }
+
+    return 0;
+
+}           
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+
+
