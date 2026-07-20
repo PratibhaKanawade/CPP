9fedd1f770e71ce113a1b6af2d783803569f8026
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-20 21:39:30 +0530

    updated ternary operator program

---
+++ ternaryoperator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int age;
+   cout<<"enter your age:"<<endl;
+   cin>>age;
+   (age>18) ?cout<<"you can vote":cout<<"you are not eligible for vote";
+    return 0;
+
+}           
