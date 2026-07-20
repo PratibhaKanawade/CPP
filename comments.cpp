33f1ebda836c59fa777370f5da217113a730ad40
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-20 23:40:43 +0530

    comments program

---
+++ comments.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //single line comment is like //
+   // int age=21;
+
+   //multiple line comment is like /*  */
+    /*cout<<age<<endl;
+    age=101;
+    cout<<age<<endl;
+    return 0;*/
+
+}           
