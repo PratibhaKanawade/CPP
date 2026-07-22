eff806b99204413cfe6471aa59533a5acadd7bca
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-22 13:27:15 +0530

    for loop example program

---
+++ forloopexample.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //here we give semicolon ; after for loop here our program not work because of writting semicilon we end loop
+   /* for(int i=1;i<=10;i++);
+    {
+    cout<<i;
+    }*/
+
+  for(int i=1;i<=10;i++)
+    {
+    cout<<"Pratibha"<<" "<<endl;
+    }
+    
+    //2nd example
+    int j;
+    cout<<"enter your j:";
+    if(cin>>j){
+        cout<<"Pratibha"<<"  "<<endl;
+    }
+
+    //3rd example
+    int k;
+    if(cout<<"hii"){
+        cout<<"Pratibha";
+    }
+    return 0;
+
+}           
