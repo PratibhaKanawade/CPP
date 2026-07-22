650657c3fc991ffd2221a2c950d3fe1365901ba5
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-22 23:40:05 +0530

    function program

---
+++ functions.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int sum(int a,int b){
+        return a+b;
+    }
+    void PrintMyName(){
+        cout<<"Kanawade";
+    }
+        
+    
+
+    int main(){
+      int ans= sum(5,10);
+      cout<<" your sum is:"<<ans<<endl;
+      
+      PrintMyName();
+
+    return 0;
+    
+}           
