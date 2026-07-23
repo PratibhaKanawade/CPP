16a8748588a360e89b2bb59ed7154a440207f4f9
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-23 23:57:35 +0530

    array program

--- array.cpp
+++ array.cpp
@@ -3,12 +3,32 @@ using namespace std;
 
 int main()
 {
+    //array declaration
+    int pk[8];
+
+    //array initialization
+    int pbk[5]={1,2,3,4,5};
+
+    //basic array
     int arr[5] = {10, 20, 30, 40, 50};
 
-    for(int i = 0; i < 5; i++)
+    for(int i = 0; i < 5; i++)//Here we show our index is from 0 to 5
     {
         cout << arr[i] << " ";
     }
+    cout<<endl;
+
+    //input in array
+    int j[5];
+    for(int j=0;j<=4;j++){
+        cout<<"enter your values:";
+        cin>>arr[j];
+    }
+    for(int j=0;j<=4;j++){
+        cout<<arr[j]<<" ";
+    }
+    
+    
 
     return 0;
 }
