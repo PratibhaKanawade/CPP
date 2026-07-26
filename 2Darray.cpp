545564646de14c9e9e7ad993eac0305b796c4f75
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-26 20:42:35 +0530

    2D array program

---
+++ 2Darray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //2D array declaration
+    int arr[3][4];  //here 3 is row       //here 4 is column
+
+    //2D array initialization
+    int arr2d[2][4]={  {1,2,3,4},  {5,6,7,8}  };
+    cout<<arr2d[0][3]<<endl;  //here we print a number from 0th row and from 4th column
+    return 0;
+
+}           
