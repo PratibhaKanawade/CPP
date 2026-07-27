e42d2aedf8febf6d115e6832849524b1143cffe4
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-07-27 22:49:16 +0530

    cout behaviour in int and char array program

---
+++ coutbehaviourinintandchar.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //cout behaviour with integer array
+    int arr[4];
+    cout<<arr<<endl;     //integer array me jab hum arr ko print karte hai tab 1st index ka address print hota hai
+
+
+    //cout behaviour with character array
+    char brr[]="Kanawade";
+    cout<<brr;     //character array me jab brr ko print karte hai tab full character array print ho jata hai
+
+
+    return 0;
+}
