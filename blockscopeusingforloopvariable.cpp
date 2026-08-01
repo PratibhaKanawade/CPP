5752cf2b39497035d69a661f422219442edaccec
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-01 23:59:59 +0530

    block scope variable example

---
+++ blockscopeusingforloopvariable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(int i=1;i<=3;i++)
+    {
+        cout << i << endl;
+    }
+
+     //cout << i;  this gives an error because i exist only inside the for loop
+
+    return 0;
+}
