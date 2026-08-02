e1bae33c5fc892c6b0302733b8d35980127feee0
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-02 13:34:15 +0530

    copying one string into another string

---
+++ stringcopy.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+    char name1[]="Pratibha";
+    char name2[20];
+    strcpy(name2,name1);
+    cout<<"name2 is:"<<name2;
+}
