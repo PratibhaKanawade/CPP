ec0d2197e0c1151a89bc4133f061ffead5724842
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-04 23:28:58 +0530

    printing specific part of your string program

---
+++ substring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name="Pratibha";
+    
+   cout<< name.substr(3,4)<<endl;  //s.substr(position,length)  //here s is name of your string
+    
+}
