d384c799b39050090a94a5de5599da92b60d4601
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-04 23:45:08 +0530

    finding string program

---
+++ findstring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name=" My name is Pratibha";
+    string temp="My";
+    cout<<name.find(temp);   //s.find(tpos)    //here s is name of your string
+    
+}
