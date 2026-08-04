5d39dc1ec91ac214697289d2a688e6c90c5eb33d
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-04 23:30:01 +0530

    comparing 2 strings program

---
+++ comparestring.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name="Pratibha";
+    string temp="Kanawade";
+    cout<<name.compare(temp)<<endl;   //s.compare(t)    //here s is name of your string
+
+    //suppose your answer is either positive or either negative then your strings are not same
+
+    string pk="cat";
+    string bk="cat";
+     cout<<pk.compare(bk);
+
+     //suppose ypur answer is 0 then your strings are equal
+    
+}
+
