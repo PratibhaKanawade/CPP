7f197597a194211c0c086caff41d431635f1e375
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-04 23:25:41 +0530

    appending(joinning) of 2 strings program

---
+++ appendstring.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string name="Pratibha";
+    string temp="Kanawade";
+    name.append(temp);   //s.append(t)      //here s is name of your string
+    
+    cout<<name<<endl;
+}
