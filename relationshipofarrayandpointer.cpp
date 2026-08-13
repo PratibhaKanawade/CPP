1d1e816fc610a7cfdd0bab31cd3d53812a0b924b
Pratibha Kanawade <pratibhakanawade5@gmail.com>
2026-08-13 23:44:36 +0530

    relationship of array and pointer program

---
+++ relationshipofarrayandpointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[]={10,20,30};
+
+    cout<<arr[0]<<endl;
+    cout<<*(arr+0)<<endl;
+    
+    cout<<arr[1]<<endl;
+    cout<<*(arr+1);
+
+    return 0;
+}
